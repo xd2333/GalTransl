ed02f0211947ab53ed26c94b8db61edd2870710c
XD2333 <99069487+XD2333@users.noreply.github.com>
2023-06-28 17:42:31 +0800

    修复修改启动器导致的一个bug

--- run_GalTransl.py
+++ run_GalTransl.py
@@ -19,14 +19,16 @@ print(f"Contributors: {CONTRIBUTORS}\n")
 
 INPUT_PROMPT = "请输入或拖入包含config.yaml的项目文件夹，或任意文件名的项目配置文件："
 
-project_dir = os.path.abspath(input(INPUT_PROMPT))
-config_file_name = CONFIG_FILENAME
-if project_dir.endswith(".yaml"):
-    config_file_name = os.path.basename(project_dir)
-    project_dir = os.path.dirname(project_dir)
-while not os.path.exists(os.path.join(project_dir, config_file_name)):
-    print(f"项目文件夹内不存在配置文件{config_file_name}，请检查后重新输入\n")
+while True:
     project_dir = os.path.abspath(input(INPUT_PROMPT))
+    config_file_name = CONFIG_FILENAME
+    if project_dir.endswith(".yaml"):
+        config_file_name = os.path.basename(project_dir)
+        project_dir = os.path.dirname(project_dir)
+    if not os.path.exists(os.path.join(project_dir, config_file_name)):
+        print(f"项目文件夹内不存在配置文件{config_file_name}，请检查后重新输入\n")
+        continue
+    break
 
 os.system("")  # 解决cmd的ANSI转义bug
 translator = BulletMenu("翻译器：", TRANSLATOR_SUPPORTED).run()

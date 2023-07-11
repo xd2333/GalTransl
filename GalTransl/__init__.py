f84947a42a3827be8609c91729b7efc4160dd381
XD2333 <99069487+XD2333@users.noreply.github.com>
2023-07-11 22:27:42 +0800

    bump version

--- GalTransl/__init__.py
+++ GalTransl/__init__.py
@@ -14,7 +14,7 @@ PROGRAM_SPLASH = """
   \____|\__,_|_| |_||_|  \__,_|_| |_|___/_|                                           
 """
 
-GALTRANSL_VERSION = "2.3.3"
+GALTRANSL_VERSION = "2.4.0"
 AUTHOR = "cx2333"
 CONTRIBUTORS = "ryank231231, Isotr0py"
 

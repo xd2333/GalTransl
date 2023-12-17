026cece6542afe0095bc24ac4695381d7ec50504
XD2333 <99069487+XD2333@users.noreply.github.com>
2023-12-17 23:32:52 +0800

    bug fix

--- GalTransl/Backend/revChatGPT/V3.py
+++ GalTransl/Backend/revChatGPT/V3.py
@@ -131,6 +131,7 @@ class Chatbot:
         if self.engine not in [
             "gpt-3.5-turbo",
             "gpt-3.5-turbo-0301",
+            "gpt-3.5-turbo-0613",
             "gpt-3.5-turbo-1106",
             "gpt-4",
             "gpt-4-0314",

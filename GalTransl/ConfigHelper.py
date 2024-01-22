20b2be18abd382ed2b9c6a177838e08517fa717b
cx2333 <snzhb233@gmail.com>
2024-01-22 14:23:26 +0800

    代理检测重试3次

--- GalTransl/ConfigHelper.py
+++ GalTransl/ConfigHelper.py
@@ -12,6 +12,7 @@ from GalTransl import (
 # from GalTransl.COpenAI import COpenAIToken
 from GalTransl.Problem import CTranslateProblem
 from asyncio import gather
+from tenacity import retry, stop_after_attempt, wait_fixed
 from httpx import AsyncClient, TimeoutException
 from time import time
 from typing import Optional
@@ -120,6 +121,7 @@ class CProxyPool:
                 (False, CProxy(i["address"], i.get("username", i.get("password"))))
             )
 
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
     async def _availablityChecker(
         self, proxy: CProxy, test_address="http://www.gstatic.com/generate_204"
     ) -> tuple[bool, CProxy]:

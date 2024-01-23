a2600c8cceb7b80b8a903996315adde0d776b964
cx2333 <snzhb233@gmail.com>
2024-01-23 19:19:53 +0800

    rename baseplugin

--- GalTransl/GTPlugin.py
+++ GalTransl/GTPlugin.py
@@ -1,19 +1,25 @@
+from configparser import SectionProxy
+from GalTransl import LOGGER
 from GalTransl.yapsy.IPlugin import IPlugin
 from GalTransl.CSentense import CSentense
 
 
-class BasePlugin(IPlugin):
+class GTPlugin(IPlugin):
 
-    def __init__(self):
+    def GTP_init(self, settings: SectionProxy):
         """
-        Make some initializations if necessary.
+        This method is called when the plugin is loaded.
+        在插件加载时被调用。
+        如果配置文件中有Settings，则会传入用于初始化插件设置。
+        :param settings: The settings for the plugin.
         """
-        super().__init__()
         pass
+
 
     def before_src_processed(self, tran: CSentense) -> CSentense:
         """
         This method is called before the source sentence is processed.
+        在post_jp没有被去除对话框和字典替换之前的处理，如果这是第一个插件的话post_jp=原始日文。
         :param tran: The CSentense to be processed.
         :return: The modified CSentense."""
         return tran
@@ -21,6 +27,7 @@ class BasePlugin(IPlugin):
     def after_src_processed(self, tran: CSentense) -> CSentense:
         """
         This method is called after the source sentence is processed.
+        在post_jp已经被去除对话框和字典替换之后的处理。
         :param tran: The CSentense to be processed.
         :return: The modified CSentense.
         """
@@ -29,6 +36,7 @@ class BasePlugin(IPlugin):
     def before_dst_processed(self, tran: CSentense) -> CSentense:
         """
         This method is called before the destination sentence is processed.
+        在post_zh没有被恢复对话框和字典替换之前的处理，如果这是第一个插件的话post_zh=原始译文。
         :param tran: The CSentense to be processed.
         :return: The modified CSentense.
         """
@@ -37,13 +45,15 @@ class BasePlugin(IPlugin):
     def after_dst_processed(self, tran: CSentense) -> CSentense:
         """
         This method is called after the destination sentence is processed.
+        在post_zh已经被恢复对话框和字典替换之后的处理。
         :param tran: The CSentense to be processed.
         :return: The modified CSentense.
         """
         return tran
-    
+
     def after_all_transl_done(self):
         """
         This method is called after all translations are done.
+        在所有文件翻译完成之后的动作，例如输出提示信息。
         """
         pass

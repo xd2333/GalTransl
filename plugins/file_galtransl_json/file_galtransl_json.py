2fcf7a98d05c83ce33ce2c3ff9e3cfb07df0660c
cx2333 <99069487+xd2333@users.noreply.github.com>
2024-07-12 08:13:24 +0800

    优化name&message格式的json的读取速度

--- plugins/file_galtransl_json/file_galtransl_json.py
+++ plugins/file_galtransl_json/file_galtransl_json.py
@@ -1,4 +1,4 @@
-import json, re
+import orjson, re
 from GalTransl import LOGGER
 from GalTransl.GTPlugin import GFilePlugin
 
@@ -21,9 +21,9 @@ class file_plugin(GFilePlugin):
         """
         if not file_path.endswith(".json"):
             # 检查不支持的文件类型并抛出TypeError
-            raise TypeError("File type not supported.")
+            raise TypeError("请检查filePlugin的配置并选择合适的文件插件.")
         with open(file_path, "r", encoding="utf-8") as f:
-            json_list = json.loads(f.read())
+            json_list = orjson.loads(f.read())
         return json_list
 
     def save_file(self, file_path: str, transl_json: list):
@@ -34,8 +34,8 @@ class file_plugin(GFilePlugin):
         :param transl_json: A list of objects same as the return of load_file().load_file提供的json在翻译message和name后的结果。
         :return: None.
         """
-        with open(file_path, "w", encoding="utf-8") as f:
-            json.dump(transl_json, f, ensure_ascii=False, indent=4)
+        with open(file_path, "wb") as f:
+            f.write(orjson.dumps(transl_json, option=orjson.OPT_INDENT_2))
 
     def gtp_final(self):
         """
